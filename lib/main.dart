859bb8c41018e3c5a8a49c41d4e2b967c6483e6b
ascentman <ascentman@ukr.net>
2023-06-01 15:10:55 +0300

    dall-e endpoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -7,39 +10,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CHAT GPT',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'CHAT GPT'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,47 +36,18 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -106,10 +61,74 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () {
+          OpenAiService.generateImage('football ball with flag of Ukraine');
+        },
+        child: const Icon(Icons.microwave_rounded),
+      ),
+    );
+  }
+}
+
+class OpenAiService {
+  static Future<void> generateImage(String prompt) async {
+    final uri = Uri.parse('https://api.openai.com/v1/images/generations');
+    try {
+      final result = await http.post(
+        uri,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization':
+              'Bearer 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'
+        },
+        body: jsonEncode({
+          'prompt': prompt,
+          'n': 5,
+          'size': "1024x1024",
+        }),
+      );
+      print(result.body);
+      if (result.statusCode == 200) {
+        print('success');
+        final jsonData = json.decode(result.body);
+        final myData = MyDataModel.fromJson(jsonData);
+        print(myData.data.first);
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
+
+class MyDataModel {
+  final int created;
+  final List<MyDataItem> data;
+
+  MyDataModel({
+    required this.created,
+    required this.data,
+  });
+
+  factory MyDataModel.fromJson(Map<String, dynamic> json) {
+    return MyDataModel(
+      created: json['created'],
+      data: List<MyDataItem>.from(
+        json['data'].map((item) => MyDataItem.fromJson(item)),
+      ),
+    );
+  }
+}
+
+class MyDataItem {
+  final String url;
+
+  MyDataItem({
+    required this.url,
+  });
+
+  factory MyDataItem.fromJson(Map<String, dynamic> json) {
+    return MyDataItem(
+      url: json['url'],
     );
   }
 }
